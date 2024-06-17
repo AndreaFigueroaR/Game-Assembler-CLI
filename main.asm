96f5fff700f119e39bb1397bc6378332a0ced7a1
Andrea Figueroa <129856581+AndreaFigueroaR@users.noreply.github.com>
2024-06-17 18:45:11 -0300

    estructura básica del programa

--- main.asm
+++ main.asm
@@ -6,7 +6,8 @@ section .data
     simboloZorro        db "X"  ;Se puede cambiar a otro simbolo reemplanzando el contenido por el simbolo ASCCI
     simboloOcas         db "O"  ;idem anterior
     rotacionTablero     db 0    ; 0->0º, 1->90º, 2->180º, 3-> 250º en sentido horario
-    estadoPartida       db 0    ;0->nadie ganò, 1-> ganò
+    estadoPartida       db 0    ;0->partidaActiva, 1-> partida terminada: ganò el Zorro, 2-> partida terminada: ganaron las ocas, 3->partida interrumpida
+    jugadorActual       db 0    ;0->turno del zorro, 1->turno de las ocas
 
 
 section .bss   
@@ -18,20 +19,18 @@ section .bss
 
 section .text
 main:
-    callRecoverGame         posicionZorro,  posicionesOcas, cantidadOcasVivas,  rotacionTablero
+    ;INICIALIZACIÓN JUEGO
+    callRecoverGame         posicionZorro,  posicionesOcas, cantidadOcasVivas,  rotacionTablero ; también debería de recuperar y guardar en su propieo archivo con sus variables la información de las estadísticas de la anterior partida, pero eso no importa en este archivo
     callCustomizeGame       simboloZorro,   simboloOcas,    rotacionTablero
+    ;MOSTRAR "INSTRUCCIONES": cómo interrumpir una partida, cómo indican sus movimientos las ocas, cómo lo hace el zorro.
 
     ;INICIA_PARTIDA
-    ;bucle
-    ;por què se interrumpe la partida: interrupciòn
-    ;mostrar porque acabo la partida y quien ganò
+    ;bucle: no olvidar guardar el contenido de registros importantes al iniciar el bucle, luego su cuerpo{recibir entrada, validar formato, validar movimiento, realizar jugada, imprimir tablero} y luego restaurar el contenido de los registros.
+    
+    ;TERMINÓ O SE INTERRUMPIÓ LA PARTIDA
+    ;Una vez se salga del bucle según por què se salió:(¿Hay algun ganador?¿Se interrumpió la partida?) 
+    ;    se muestra quién fue el ganador, se muestran las estadísticas de la partida 
+    ;    o
+    ;    se escriben los detalles del estado del juego actual en un archivo (incluyendo el conteo que se lleva para las estadisticas que se muestran cuando se gana una partida, así como el estado de las variables globales del main)
 
-
-
-
-;*****************************************************************************
-;   RUTINAS INTERNAS
-;*****************************************************************************
-
-;*****************************************************************************
-
+    ;fin del programa
