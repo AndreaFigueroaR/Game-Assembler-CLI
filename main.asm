9ff78739d540372bd51e00da219a97f30892c82d
Jesabel Pugliese <129856283+jesapugliese@users.noreply.github.com>
2024-06-18 20:09:52 -0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -5,32 +5,35 @@ global main
 section .data
     simboloZorro        db "X"  ;Se puede cambiar a otro simbolo reemplanzando el contenido por el simbolo ASCCI
     simboloOcas         db "O"  ;idem anterior
-    rotacionTablero     db 0    ; 0->0º, 1->90º, 2->180º, 3-> 250º en sentido horario
-    estadoPartida       db 0    ;0->partidaActiva, 1-> partida terminada: ganò el Zorro, 2-> partida terminada: ganaron las ocas, 3->partida interrumpida
-    jugadorActual       db 0    ;0->turno del zorro, 1->turno de las ocas
-
 
 section .bss   
     cantidadOcasVivas   resb 1;            tamaño a a discusiòn
-    posicionesOcas      times 17 resb 2;   [[1,3],[1,4],...], [fil,col] ->17 pares de coordenadas
-    posicionZorro       resb 2;            [5,4]->posicion inicial del zorro antes del movimiento (equivalente a 5D)
-    posicionNueva       resb 2;            [4,4]->nueva posicion (decodificada a partir de la entrada "4D")
-
+    posicionesOcas      times 17 resq 2;   [[0,1],[1,2],...], [X,Y] ->17 pares de coordenadas
+    posicionZorro       resq 2;            [5,4]->posicion del zorro 
+    comando             resq 2;
+    jugadorActual       db 1               ;0->turno del zorro, 1->turno de las ocas
+    rotacionTablero     db 1               ; 0->0º, 1->90º, 2->180º, 3-> 250º en sentido horario
+    estadoPartida       db 1               ;0->partidaActiva, 1-> partida terminada: ganò el Zorro, 2-> partida terminada: ganaron las ocas, 3->partida interrumpida
+    estadisticas        db 8               ;es como un vector donde cada posicion corresponde a la cantidad de movimientos en cada direccion. [arriba, abajo, izq, der, arriba-izq, arriba-der, abajo-izq, abajo-der]
 
 section .text
 main:
-    ;INICIALIZACIÓN JUEGO
+;   INICIALIZACIÓN JUEGO
     callRecoverGame         posicionZorro,  posicionesOcas, cantidadOcasVivas,  rotacionTablero ; también debería de recuperar y guardar en su propieo archivo con sus variables la información de las estadísticas de la anterior partida, pero eso no importa en este archivo
     callCustomizeGame       simboloZorro,   simboloOcas,    rotacionTablero
-    ;MOSTRAR "INSTRUCCIONES": cómo interrumpir una partida, cómo indican sus movimientos las ocas, cómo lo hace el zorro.
 
-    ;INICIA_PARTIDA
-    ;bucle: no olvidar guardar el contenido de registros importantes al iniciar el bucle, luego su cuerpo{recibir entrada, validar formato, validar movimiento, realizar jugada, imprimir tablero} y luego restaurar el contenido de los registros.
-    
-    ;TERMINÓ O SE INTERRUMPIÓ LA PARTIDA
-    ;Una vez se salga del bucle según por què se salió:(¿Hay algun ganador?¿Se interrumpió la partida?) 
-    ;    se muestra quién fue el ganador, se muestran las estadísticas de la partida 
-    ;    o
-    ;    se escriben los detalles del estado del juego actual en un archivo (incluyendo el conteo que se lleva para las estadisticas que se muestran cuando se gana una partida, así como el estado de las variables globales del main)
+continuarJugando:
+    call                    clear ; esto ponerlo con al funcion de c
+    imprimirTabla           cantidadOcasVivas, posicionesOcas, posicionZorro, simboloZorro, simboloOcas, rotacionTablero
+    mGets                   comando
+    realizarJugada          cantidadOcasVivas, posicionesOcas, posicionZorro, comando, jugadorActual, estadisticas
+    resultadoJuego          cantidadOcasVivas, posicionesOcas, posicionZorro, jugadorActual, estadoPartida
+    cmp                     byte[estadoPartida],0
+    je                      continuarJugando
 
-    ;fin del programa
+;   FIN DEL JUEGO
+    imprimirMsgFinJuego     estadoPartida ; imprimir quien gano y asi
+    preguntarGuardarPartida cantidadOcasVivas, posicionesOcas, posicionZorro, comando, jugadorActual, rotacionTablero, estadoPartida, estadisticas, simboloZorro, simboloOcas
+    mostrarEstadisticas     estadisticas
+
+    ret
