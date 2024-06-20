7423b4c4e4d662a54a7f4fdb6feb1d8a96f50550
leticiafrR <129856789+leticiafrR@users.noreply.github.com>
2024-06-20 12:27:15 -0300

    modularizando print_cruz

--- print_cruz.asm
+++ print_cruz.asm
@@ -1,69 +1,7 @@
+%include "macros_cruz.asm"
 global imprimirTablero
 extern printf
 extern puts
-%macro cambiarSimbA 1
-    ;cambia a què apunta punteroSimb al ròtulo recibido
-    mov     r8,%1
-    mov     [punteroSimb],r8
-%endmacro
-%macro esPar? 1
-    mov     rax,[%1]
-    ;verifica el menos significativo
-    and     rax,1
-    mov     [esPar],rax
-%endmacro
-%macro mostrarSimb 0
-    ;muestra el simbolo apuntado en punteroSimb
-    mov     rdi,formato
-    mov     rsi,[punteroSimb]
-    sub     rsp,8
-    call    printf
-    add     rsp,8
-%endmacro
-
-%macro ponerPuts 0
-    sub     rsp,8
-    call    puts
-    add     rsp,8
-%endmacro
-
-%macro mostrarUbiFilYActualizar 0
-    ;PRE: ubiFil debe ser la fila actual
-    ;POST: muestra ubiFil y lo incrementa
-    mov     rdi,formatoUbiFil
-    mov     rsi,[ubiFil]
-    sub     rsp,8
-    call    printf
-    add     rsp,8
-    inc     qword[ubiFil]
-%endmacro
-%macro mostrarUbiCol 0
-    mov             rdi,ubiCol
-    ponerPuts
-    cambiarSimbA    espacio     ;espacio adicional para mostrar ubiFil al lateral
-    mostrarSimb                 ;doble espacio para separar el tablero de ubiFil
-    mostrarSimb 
-%endmacro
-
-%macro guardarEstadoJuego 0
-    ;POST: guardar todos los vectores recibidos por registros RDI y RSI
-    mov         [rotacionTablero],rdx
-    
-    mov         [dirBaseVector],rdi
-    mov         qword[cantElemVector],36
-    mov         qword[dirDestinoVector],infoOcas
-    sub         rsp,8
-    call        guardarVector
-    add         rsp,8
-
-    mov         [dirBaseVector],rsi
-    mov         qword[cantElemVector],3
-    mov         qword[dirDestinoVector],infoZorro
-    sub         rsp,8
-    call        guardarVector
-    add         rsp,8
-%endmacro
-
 
 section     .data
     ;movimientos en la matriz
@@ -87,9 +25,9 @@ section     .bss
     infoZorro                   times 0 resb 
         simboloZorro            resq    1
         posicionZorro           times   1   resq    2
-    rotacionTablero             resq    1   ;0ª->1,270ª->2,90ª->-1,180ª->-2
+    rotacionTablero         resq    1   ;0ª->1,270ª->2,90ª->-1,180ª->-2
         
-    ;auxiliares de impresion y copia de datos
+    ;auxiliares de impresion
     esPar               resq    1   ;0 si es par, 1 si es impar
     punteroSimb         resq    1   ;4 bytes para un puntero
     desplazVector       resq    1
@@ -132,6 +70,7 @@ _mostrarSimbSiguiente:
 fin:
     cambiarSimbA    salto
     mostrarSimb
+    
 ret
 
 ;_________________RUTINAS INTERNAS_________________________
