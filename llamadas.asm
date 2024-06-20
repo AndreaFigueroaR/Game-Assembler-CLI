edcc5bdd4a87f7020b43dca56c39ed2a78f4fb5e
leticiafrR <129856789+leticiafrR@users.noreply.github.com>
2024-06-20 10:20:11 -0300

    Update llamadas.asm

--- llamadas.asm
+++ llamadas.asm
@@ -52,7 +52,7 @@ section     .data
 
     mov     RDI,%1      ;dirInfoOcas
     mov     RSI,%2      ;dirInfoZorro
-    mov     RDX,%3    ;rotacion
+    mov     RDX,[%3]    ;rotacion
     
     sub     rsp,8
     call    imprimirTablero
