c4f89c2092e029d4186b27d964e6722e78af03d3
leticiafrR <129856789+leticiafrR@users.noreply.github.com>
2024-06-20 19:44:07 -0300

    Update macros_cruz.asm

--- macros_cruz.asm
+++ macros_cruz.asm
@@ -1,18 +1,34 @@
+%macro ubicarPersonajes 0
+    ;ubicando zorro
+    sub     rsp,8
+    call    coincidirZorro
+    add     rsp,8
+    ;ubicando ocas
+    mov     qword[desplazVector],0
+    mov     rcx,[cantOcasVivas]
+    sub     rsp,8
+    call    coincidirOcas
+    add     rsp,8
+%endmacro
+
 %macro cambiarSimbA 1
     ;cambia a què apunta punteroSimb al ròtulo recibido
     mov     r8,%1
     mov     [punteroSimb],r8
 %endmacro
+
 %macro esPar? 1
     mov     rax,[%1]
     ;verifica el menos significativo
     and     rax,1
     mov     [esPar],rax
 %endmacro
+
 %macro mostrarSimb 0
     ;muestra el simbolo apuntado en punteroSimb
     mov     rdi,formato
     mov     rsi,[punteroSimb]
+    mov     byte[rsi+1],0   ;agregando fin de strings
     sub     rsp,8
     call    printf
     add     rsp,8
@@ -34,6 +50,7 @@
     add     rsp,8
     inc     qword[ubiFil]
 %endmacro
+
 %macro mostrarUbiCol 0
     mov             rdi,ubiCol
     ponerPuts
@@ -45,10 +62,10 @@
 %macro guardarEstadoJuego 0
     ;POST: guardar todos los vectores recibidos por registros RDI y RSI
     mov         [rotacionTablero],rdx
-    
     mov         [dirBaseVector],rdi
     mov         qword[cantElemVector],36
     mov         qword[dirDestinoVector],infoOcas
+
     sub         rsp,8
     call        guardarVector
     add         rsp,8
@@ -56,7 +73,30 @@
     mov         [dirBaseVector],rsi
     mov         qword[cantElemVector],3
     mov         qword[dirDestinoVector],infoZorro
+
     sub         rsp,8
     call        guardarVector
     add         rsp,8
+%endmacro
+
+%macro rotarPoscionesPersonajes 0
+    ;rotando zorro
+    mov     r10,[posicionZorro]  ;fil
+    mov     r9,8
+    mov     r11,[posicionZorro+r9]  ;col
+    mov     r9,0
+    mov     qword[dirBaseVector],posicionZorro
+
+    sub     rsp,8
+    call    rotarPosicion
+    add     rsp,8
+
+    ;rotando Ocas
+    mov     qword[dirBaseVector],posicionesOcas
+    mov     qword[desplazVector],0
+
+    sub     rsp,8
+    call    rotarPosicionesOcas
+    add     rsp,8
+
 %endmacro
