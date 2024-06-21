90f9676be15162ed648bfbf41e3027053840747b
leticiafrR <129856789+leticiafrR@users.noreply.github.com>
2024-06-21 17:08:34 -0300

    Update macrosRealizarJugada.asm

--- macrosRealizarJugada.asm
+++ macrosRealizarJugada.asm
@@ -1,18 +1,39 @@
 
-%macro  guardarDatos 0
-    mov     [dirInfoOcas],          rdi
-    mov     [dirPosicionZorro],     rsi
-    mov     [dirJugadorActual],     rcx
-    mov     r9,                     [rcx]
-    mov     [juagadorActual],       r9
-    mov     [dirEstadisticas],      r8
-    ;guardando coordenadas
-    mov         [dirBaseVector],rdx
-    mov         qword[cantElemVector],4
-    mov         qword[dirDestinoVector],infoCoordenadas
+%macro copiarVector 3
+    ;PRE: recibe el tamaño a copiar, la dir del vector DE DONDE se copiarà, y dir del vector DONDE se copiarà 
+    mov         qword[cantElemVector],%1
+    mov         [dirBaseVector],%2
+    mov         qword[dirDestinoVector],%3
 
     sub         rsp,8
     call        guardarVector
     add         rsp,8
 
+%endmacro
+
+%macro  guardarDatos 0
+    mov     [dirPosicionZorro],     rsi
+    mov     [dirEstadisticas],      r8
+    mov     [dirCantidadOcasVivas], rdi
+    mov     r10,                    [rdi]
+    mov     [cantidadOcasVivas],    r10
+    add     rdi,                    16  ;<-offset para dir posicionesOcas
+    mov     [dirPosicionesOcas],    rdi
+
+    mov     [dirJugadorActual],     rcx
+    mov     r10,                     [rcx]
+    mov     [juagadorActual],       r10
+    ;guardando coordenadas
+    copiarVector                    4,rdx,infoCoordenadas
+%endmacro
+
+%macro buscarOcaPorCoordenadas 1
+    ;PRE: Recibe el rotulo de la posicion que se quiere buscar. Ejem: buscarOcaPorCoordenadas coordenadasOrigen
+    ;POST:Deja en desplazVector el desplazamiento desde el inicio del vector
+    mov                 rcx,[cantidadOcasVivas]
+    mov                 qword[desplazVector],0
+    copiarVector        2,%1,coordenadasBuscadas
+    sub                 rsp,8
+    call                buscarOca
+    add                 rsp,8
 %endmacro
