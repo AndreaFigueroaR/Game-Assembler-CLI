1b2b054cf0e7a63c5acfcede4ae4f24bff151119
leticiafrR <129856789+leticiafrR@users.noreply.github.com>
2024-06-21 13:33:38 -0300

    Update main.asm separaciòn info zorro

--- main.asm
+++ main.asm
@@ -18,8 +18,9 @@ section .data
     
 section .bss   
     infoOcas                times 18 resq 2      ; infoOcas es un vector de la forma: [cantidadOcasVivas, simboloOcas, posFilOca1, posColOca1, ..., posFilOcaN, posColOcaN], donde 0 <= N <= 17
-    infoZorro               times 3 resq 1       ; infoZorro es un vector de la forma: [simboloZorro, posFilZorro, posColZorro]
-    jugadorActual           resq 1               ; Valor del jugador actual. 0 -> turno del zorro, 1 -> turno de las ocas.
+    infoZorro               times 0  resb        ; infoZorro es un vector de la forma: [simboloZorro, posFilZorro, posColZorro]
+        simboloZorro                 resq    1
+        posicionZorro                times   1   resq    2      jugadorActual           resq 1               ; Valor del jugador actual. 0 -> turno del zorro, 1 -> turno de las ocas.
     rotacionTablero         resq 1               ; Valor de rotacion de la tabla en sentido antihorario. 0 -> 0º, 1 -> 90º, 2 -> 180º, 3 -> 250º
     estadoPartida           resq 1               ; Valor del estado de la partida. 0 -> partidaActiva, 1 -> partida terminada: ganó el Zorro, 2 -> partida terminada: ganaron las ocas, 3 -> partida interrumpida
     estadisticas            times 8 resq 1       ; Vector donde cada posicion corresponde a la cantidad de movimientos en cada direccion: [arriba, abajo, izq, der, arriba-izq, arriba-der, abajo-izq, abajo-der]
