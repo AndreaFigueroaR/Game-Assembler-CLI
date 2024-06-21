ebe0cece5b02db129975e978629a0e9180a327eb
leticiafrR <129856789+leticiafrR@users.noreply.github.com>
2024-06-21 17:08:04 -0300

    Update realizarJugada.asm

--- realizarJugada.asm
+++ realizarJugada.asm
@@ -15,23 +15,35 @@ section     .bss
     infoCoordenadas         times   0   resb        ; Vector de la forma: [filOrigen, colOrigen, filDestino,colDestino]
         coordenadasOrigen               resq 2
         coordenadasDestino              resq 2
+    cantidadOcasVivas       resq    1
     ;punteros
-    dirInfoOcas             resq    1
+    dirCantidadOcasVivas    resq    1   
+    dirPosicionesOcas       resq        ;[posFilOca1, posColOca1, ..., posFilOcaN, posColOcaN]
     dirPosicionZorro        resq    1
     dirJugadorActual        resq    1
     dirEstadisticas         resq    1
-    ;dirPosJugadorActual     resq    1
       
-
     ;auxiliares
-    desplazVector       resq    1
-    cantElemVector      resq    1
-    dirBaseVector       resq    1
-    dirDestinoVector    resq    1
+    desplazVector                   resq    1
+    cantElemVector                  resq    1
+    dirBaseVector                   resq    1
+    dirDestinoVector                resq    1
+    coordenadasBuscadas times   0   
+        filBuscada                  resq    1
+        colBuscada                  resq    1
 
 
 realizarJugada:
     guardarDatos
+    ;moviendo jugador actual
+    cmp                     qword[juagadorActual],0 ;->0 si es turno del zorro
+    jne                     cambiarPosOcas
+    mov                     rax,[dirPosicionZorro]
+    copiarVector            2,coordenadasDestino,rax
+    jmp                     ejecutarMovimiento
+    cambiarPosOcas:
+    buscarOcaPorCoordenadas coordenadasOrigen ;-
+    ;necesito encontrar la direcciòn de la posiciòn de la oca que tiene  la misma posicion que coordenadas origen y a esa direcciòn llevarme la coordenada destino
     ejecutarMovimiento:
     ;se actualiza la info del jugador actual en base del las coordenadas copiadas
 
@@ -54,6 +66,25 @@ realizarJugada:
 ;__________________________________________________________
 ;**********************************************************
 
+buscarOca::
+    ;si ya la encontrè no sigo buscando=>el desplazamiento me dirà la direcciòn que le tengo que mnadar a copiar vector
+    mov             r8,[desplazVector]
+    mov             r9,[dirPosicionesOcas+r8];->tengo la fila
+    ;si la fila no coincide->sgt oca
+    cmp             r9,[filBuscada]
+    jne             sgtOca
+    ;si la columna no coincide->sgt oca
+    add             r8,8
+    mov             r9,[dirPosicionesOcas+r8];->tengo la columna
+    cmp             r9,[colBuscada]
+    jne             sgtOca
+    ;encontrè coincidencia=>salgo del loop
+    jmp             ocaEncontrada
+    sgtOca:
+    add             qword[desplazVector],16;->compara de a pares
+    loop buscarOca:
+    ocaEncontrada:
+    ret
 ;__________________________________________________________
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 guardarVector:
