e70729a7ab55a662a865a3e56c70877576b1dfc8
leticiafrR <129856789+leticiafrR@users.noreply.github.com>
2024-06-21 15:24:14 -0300

    Update realizarJugada.asm

--- realizarJugada.asm
+++ realizarJugada.asm
@@ -3,32 +3,76 @@
 ;luego se tiene que verificar que se halla acabado el turno del jugador actual (si son ocas siempre dura solo un turno, si es el zorro se debe verificar una )
 
 %include "llamadas.asm"
+%include "macrosRealizarJugada.asm"
+
 global realizarJugada
 section     .data
     murioOca            db      0
 
 section     .bss
-    dirPosicionOrigen   resq    1;->si es que el jugador actual es el zorro aqui copio el valor de la posicion que indica su vector (antes de editarlo) y en base a la posicionDestino que dejò el procesarComando puedo asignarle su nueva posicion de destino
-    dirPosicionDestino  resq    1;
-    jugadorActual       resq    1;
+    ;por dato
+    jugadorActual           resq    1   ;0 -> turno zorro, 1 -> turno ocas.
+    infoCoordenadas         times   0   resb        ; Vector de la forma: [filOrigen, colOrigen, filDestino,colDestino]
+        coordenadasOrigen               resq 2
+        coordenadasDestino              resq 2
+    ;punteros
+    dirInfoOcas             resq    1
+    dirPosicionZorro        resq    1
+    dirJugadorActual        resq    1
+    dirEstadisticas         resq    1
+    ;dirPosJugadorActual     resq    1
+      
+
+    ;auxiliares
+    desplazVector       resq    1
+    cantElemVector      resq    1
+    dirBaseVector       resq    1
+    dirDestinoVector    resq    1
+
 
 realizarJugada:
-    salvarParametros:
-        mov [jugadorActual],        RDI
-        mov [dirPosicionOrigen],    RSI
-        mov [dirPosicionDestino],   RDX
-        mov [dirInfoZorro],         R8
-        mov [dirInfoOcas],          R9
-
+    guardarDatos
     ejecutarMovimiento:
+    ;se actualiza la info del jugador actual en base del las coordenadas copiadas
+
     ;si el jugador actual es el zorro se debe guardar lo que tenga como posicion actual en su info en la parte de posicionOrigen
     
     ;si es oca-> se cambia la posicion actual de la oca que indiue que està en el mismo liugar que la posicion de origen... y nada màs
     ;si es zorro-> si la nueva posicion es de un salto entonces se debe encontrar la oca que este en la posicion en el medio de estas(oca comida), se pisa la su posicion con la de la ultima oca viva, se disminuye la cantidad de ocas vivas, se cambia la posicion del zorro a la que indica la posicionDestino y se setea como 1 a "murioOca".
     ;               si no era un salto solo se cambia la psocion del zorro a la posicion indicada en posicionDestino
     
-    ;ejecutadas las consecuencias del movimiento sobre el juego:        
-    ;si murioOca==0 -> cambiamos de jugadorActual al opuesto
-    ;si  murioOca!=0 -> se deja el jugadorActual
+    ;ejecutadas las consecuencias del movimiento sobre el juego: 
+    ;llamamos a estadisticas y actualizamos jugadorActual:       
+        ;si murioOca==0 -> cambiamos de jugadorActual al opuesto
+        ;si  murioOca!=0 -> se deja el jugadorActual
 
     ;fin
+
+;__________________________________________________________
+;**********************************************************
+;                   RUTINAS INTERNAS
+;__________________________________________________________
+;**********************************************************
+
+;__________________________________________________________
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+guardarVector:
+    ;PRE: cantElemVector, dirBaseVector, dirDestinoVector inicializados
+    mov         qword[desplazVector],0
+    mov         r10,[desplazVector]
+    mov         r9,qword[cantElemVector]
+    copiarSgt:
+    cmp         qword[cantElemVector],0
+    je          endCopiado
+
+    mov         r10,[desplazVector]
+    mov         rdi,[dirBaseVector]
+    mov         rax,[rdi+r10]
+    mov         rbx,[dirDestinoVector]
+    mov         [rbx+r10],rax
+
+    add         qword[desplazVector],8
+    dec         qword[cantElemVector]
+    jmp         copiarSgt
+    endCopiado:
+    ret
