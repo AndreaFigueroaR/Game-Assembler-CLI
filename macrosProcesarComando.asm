d72659981ae553d3e3e12860ac3cb7124633c0c6
AndreaFigueroaR <afigueroa@fi.uba.ar>
2024-06-22 00:12:37 -0300

    update

--- macrosProcesarComando.asm
+++ macrosProcesarComando.asm
@@ -2,6 +2,37 @@ macrosProcesarComando.asm
 ;*****************************************************************************
 ; MACROS PARA VALIDACION DE DATOS
 ;*****************************************************************************
+%macro guardarParametros 0
+    mov [jugadorActual],        RDI
+    mov [dirPosicionOrigen],    RSI
+    mov [dirPosicionDestino],   RDX
+    mov [dirEstadoPartida],     RCX
+    mov [dirInfoZorro],         R8
+    mov [dirInfoOcas],          R9
+%endmacro
+
+%macro pedirMovimiento 0
+    mov     RDI,            mensajePedirMovZorro
+    cmp     qword[jugadorActual],   0
+    je      %%imprimir
+    mov     RDI,            mensajePedirMovOca
+
+%%imprimir:
+    mPuts
+    mov     RDI,            input
+    mGets
+%endmacro
+
+%macro compararInput 1
+;lleva a cabo el strcmp y deja en el RAX el resultado de la comparacion
+;recibe la direccion del comando con quien se debe comparar
+    mov     RDI,            input
+    mov     RSI,            %1
+    mStrcmp
+%endmacro
+
+
+
 %macro apruebaValidacionTotal 0
     mov byte[inputValido],  'S'
     jmp finValidacion
@@ -20,6 +51,40 @@ macrosProcesarComando.asm
     mov RSI,    %2;
     mov RDX,    %3;
     mov RCX,    %4;
+%endmacro
+%macro setParametrosScanCuatroDatos 0
+    mov     R8,     [dirPosicionOrigen]
+    add     R8,     8
+    mov     R9,     [dirPosicionDestino]
+    add     R9,     8
+    movSeisParametros       input,  formatoMovimientoOca,   qword[dirPosicionOrigen],   R8,     qword[dirPosicionDestino],  R9
+%endmacro
+
+%macro setParametrosScanDosDatos 0
+    mov     R9,             [dirPosicionDestino]
+    add     R9,     8
+    movCuatroParametros     input,  formatoMovimientoOca,   qword[dirPosicionDestino],  R9
+%macro 
+
+%macro guardarDatosOrigen 0
+;inicializa en memoria los valores de las coordenadas de origen X e Y
+    mov     R8,             [dirPosicionOrigen]
+    mov     R9,             [R8]    
+    mov     [x_origen],     R9
+    mov     R10,            [R8+8]  
+    mov     [y_origen],     R10
+
+;inicializa en memoria los valores de las coordenadas de destino X e Y
+    mov     R8,             [dirPosicionDestino]
+    mov     R9,             [R8]                
+    mov     [x_destino],    R9
+    mov     R10,            [R8+8]   
+    mov     [y_destino],    R10
+
+;guardo la cantidad de ocas vivas
+    mov     RSI, [dirInfoOcas]
+    mov     RDI,[RSI]
+    mov     [n_ocas],RDI
 %endmacro
 
 %macro dirNumEnRango 3
@@ -58,7 +123,7 @@ macrosProcesarComando.asm
 %endmacro
 
 ;tendria que recibir la direccion donde inicia el vector de posicion(origen o destino)
-%macro chequearPosicionEnTabla 1
+%macro chequearPosEnCruz 1
     dirNumEnRango   %1, 1,  7
 
     mov     R8,     %1
@@ -99,4 +164,113 @@ macrosProcesarComando.asm
     mov qword[R8],7
 %%finMacro:
 
+%endmacro
+;probado
+%macro buscarCoincidenciaCoordenadas 5
+;DEJA LOS RESULTADOS DE LA CUENTA EN EL RAX Y EL INDICE DE COINCIDENCIA EN R8
+    ; %1 -> Dirección de la matriz(vector de tuplas)
+    ; %2 -> Número de tuplas en la matriz
+    ; %3 -> X a buscar
+    ; %4 -> Y a buscar
+    ; %5 -> para parar al encontrar la primera coincidencia 0->no, 1->si
+
+    mov RSI, %1            ; Dirección base en RSI
+    mov RCX, %2            ; Contador con el número de ocas
+    mov RBX, %3            ; X a buscar en RBX
+    mov R15, %4            ; Y a buscar en R15
+    mov R14, %5            ; 0 contar todas la apariciones, 1 hasta encontrar la primera
+
+    xor RDI, RDI           ; Inicializo el indice actual (i) en RDI
+    xor RAX, RAX           ; Contador de coincidencias en RAX
+    mov R8, -1             ; Inicializa R8 con -1 para manejar la primera coincidencia
+
+%%siguienteTupla:
+    cmp RCX, 0             
+    je  %%fin              
+
+    imul    R12,    RDI,    16      ; r12= i*16
+    mov     R9,     [RSI + R12]     ; Carga X_i en R9
+    mov     R11,    [RSI + R12 + 8] ; Carga Y_i en R11
+
+    cmp     R9,     RBX 
+    jne     %%noIgual
+    cmp     R11,    R15             
+    jne     %%noIgual               
+    inc     RAX                     
+    cmp     R8,     -1              
+    jne     %%noIgual 
+
+    mov     R8,     RDI 
+
+    cmp     R14,    1
+    je      %%fin 
+
+%%noIgual:
+    inc     RDI    
+    loop    %%siguienteTupla    
+
+%%fin:
+%endmacro
+
+%macro unaOcaEnOrigen 0
+    mov RSI,                qword[dirInfoOcas];
+    add RSI,                16
+    buscarCoincidenciaCoordenadas    RSI,    qword[n_ocas],     qword[x_origen],     qword[y_origen],    1
+    cmp RAX,                1
+    jne finValidacion
+%endmacro
+
+%macro sinOcasEnDestino 0
+;MAL deberia de pasarle de primer parametro la Dirección de la matriz donde inicien las ocas
+    mov RSI,                qword[dirInfoOcas];
+    add RSI,                16
+    buscarCoincidenciaCoordenadas   RSI,    qword[n_ocas],     qword[x_destino],     qword[y_destino],    1
+    cmp RAX,                0
+    jne finValidacion
+%endmacro
+
+%macro sinZorroEnDestino 0
+    ;deberia de fijarse que la 
+    mov RSI,                qword[dirInfoZorro]
+    add RSI,                8
+    buscarCoincidenciaCoordenadas   RSI,            1,         qword[x_destino],     qword[y_destino],    1
+    cmp RAX, 0
+    jne finValidacion
+
+%endmacro
+;%macro validarDifUno 2
+;recibe los contenidos de X_Dest y de X_Or
+;    mov RAX,%1
+;    mov RBX,%2
+;    sub RAX,RBX
+;    cmp RAX,1
+;    ;deja el resultado de la comparacion de la diferencia en el registro con las flags
+;%endmacro
+
+%macro movimientoAdelanteOCostado 0
+;suponiendo que se mueve adelante
+    mov RAX, [x_destino]
+    cmp RAX, [x_origen]
+    je %%validarMovCostado
+    ;si no son iguales entonces debe suceder que la resta del destino menos el de origen sea solo 1 
+    ;necesito quitarle a RAX (que tiene el X de destino) 
+    sub RAX,[x_origen]
+    cmp RAX,1
+    jne finValidacion
+    ;si la diferenciasi fue de uno ahora debo comparar las coordenadas Y
+    mov RAX, [y_destino]
+    cmp RAX, [y_origen]
+    jne finValidacion
+    apruebaValidacionTotal
+%%validarMovCostado:
+    mov RAX, [y_destino]
+    sub RAX, [y_origen];la resta entre y destino menos y origen esta en el rax
+    cmp RAX,1
+    jne %%izquierda
+    ;si es que no son iguales -> salteo la aprovacionel movimiento si es valido
+    apruebaValidacionTotal
+%%izquierda:
+    cmp RAX,-1
+    jne finValidacion
+    apruebaValidacionTotal
 %endmacro
--- procesarComando.asm
+++ procesarComando.asm
@@ -9,119 +9,110 @@ section     .data
     mensajePedirMovOca      db "MOVIMIENTO OCA: ",0
     mensajePedirMovZorro    db "MOVIMIENTO ZORRO: ",0
     mensajeInputInvalido    db "Error con la instruccion recibida, por favor indique una accion valida.",0; usado para cuando no tiene el formato de un movimiento del jugador actual o no representa el comando para interrumpir la partida
-    
-    ;variables auxiliares
-    inputValido                 db 'N'
+    ;formato entrada
     formatoMovimientoOca        db "%li%c->%li%c",0
     formatoMovimientoZorro      db "%li%c",0
-    comandoInterrupcion         db "--guardar partida",0
+    comandoInterrupcion         db "--interrumpir partida", 0
+    comandoGuardar              db "--guardar partida",     0
+
+    ;variables auxiliares
+    inputValido                 db 'N'
+    
 
 section     .bss
     input                       resb 20;reservo 20 bytes para la entrada del imput
-    ;PARAMETROS SALVADOS
+    ;DATOS POR VALOR
     jugadorActual               resq 1; Valor del jugador actual. 0 -> turno del zorro, 1 -> turno de las ocas.
+    coordenadasOrigen           times   0   resb
+        x_origen                            resq 1;
+        y_origen                            resq 1;
+    coordenadasDestino          times   0   resb      
+        x_destino                           resq 1;
+        y_destino                           resq 1; 
+    n_ocas                      resq 1;
+
+    ;DATOS POR REFERENCIA
     dirPosicionOrigen           resq 1; donde dejarà las coordenadas de origen
     dirPosicionDestino          resq 1; donde dejarà las coordenadas de destino
     dirEstadoPartida            resq 1 
-    dirInfoZorro                resq 1
+    dirInfoZorro                resq 1;[simboloZorro, posFilZorro, posColZorro]
     dirInfoOcas                 resq 1; infoOcas es un vector de la forma:
     ;                                   [cantidadOcasVivas, simboloOcas, posFilOca1, posColOca1, ..., posFilOcaN, posColOcaN], donde 0 <= N <= 17
-
+     
 
 section     .text
 
 procesarComando:
-    
-    mov [jugadorActual],        RDI
-    mov [dirPosicionOrigen],    RSI
-    mov [dirPosicionDestino],   RDX
-    mov [dirEstadoPartida],     RCX
-    mov [dirInfoZorro],         R8
-    mov [dirInfoOcas],          R9
+    guardarParametros
 
-lecturaInput:
+pedirInput:
+    pedirMovimiento
 
-    mov RDI,mensajePedirMovZorro
-    cmp qword[jugadorActual],0
-    je mostrarTurno
-    mov RDI,mensajePedirMovOca
-    mostrarTurno:
-    mPuts
+validarInterrupcion:
+    compararInput comandoInterrupcion
+    cmp     EAX,            0
+    jne     validarGuardarPartida
+    mov     RAX,            [dirEstadoJuego]
+    mov     qword[RAX],     3
+    apruebaValidacionTotal 
 
-    mov RDI,                input
-    mGets
-
-validacionInterrupcion:
-    mov RDI,                input
-    mov RSI,                comandoInterrupcion
-    mStrcmp
-    cmp EAX,                0
-    jne                     validarFormatoMovimiento
-    mov RAX,                [dirEstadoJuego]
-    mov qword[RAX],         3
-
-    apruebaValidacionTotal  
+validarGuardarPartida:
+    compararInput comandoGuardar
+    cmp     EAX,            0
+    jne     validarFormatoMovimiento
+    ;aqui se llamaria a guardar partida
+    jmp     pedirInput
 
 validarFormatoMovimiento:
-    cmp         qword[jugadorActual],   0
-    je          movimientoZorro
+    cmp     qword[jugadorActual],   0
+    je      movimientoZorro
 
-    movimientoOca:
-        mov     R8,             [dirPosicionOrigen]
-        add     R8,     8
-        mov     R9,             [dirPosicionDestino]
-        add     R9,     8
-        movSeisParametros       input,  formatoMovimientoOca,   qword[dirPosicionOrigen],   R8,     qword[dirPosicionDestino],  R9
-        mSscanf
-        cmp     EAX,    4
-        jne     finValidacion
+movimientoOca:
+    setParametrosScanCuatroDatos
+    mSscanf
+    cmp     EAX,            4
+    jne     finValidacion
+    chequearPosEnCruz       [dirPosicionOrigen]
+    chequearPosEnCruz       [dirPosicionDestino]
+    jmp     validacionLogicaMovOca
 
-        chequearPosicionEnTabla [dirPosicionOrigen]
-        chequearPosicionEnTabla [dirPosicionDestino]
-
-        jmp validacionLogicaMovOca
-    movimientoZorro:
-        mov     R9,             [dirPosicionDestino]
-        add     R9,     8
-        movCuatroParametros     input,  formatoMovimientoOca,   qword[dirPosicionDestino],  R9
-        mSscanf
-        cmp     EAX,    2
-        jne     finValidacion
-
-        chequearPosicionEnTabla [dirPosicionDestino]
-        jmp     validacionLogicaMovZorro
+movimientoZorro:
+    setParametrosScanDosDatos
+    mSscanf
+    cmp     EAX,            2
+    jne     finValidacion
+    chequearPosEnCruz       [dirPosicionDestino]
+    jmp     validacionLogicaMovZorro
 
 validacionLogicaMovOca:
-    traducirLetra   [dirPosicionOrigen]
-    traducirLetra   [dirPosicionDestino]
-    ;1.-verificar que hay una oca con las mismas coordenadas de origen
-    mov     RSI,    [dirInfoOcas]
-    mov     RDI,    RSI                 ;RDI contiene la direccion donde esta el numero (qword) de ocas vivas
-    add     RSI,    16                  ;RSI contiene la direccion de inicio del vector
-    
-    mov     R8,     [dirPosicionOrigen]
-    mov     R9,     [R8]                ;R9 contiene X buscado
-    mov     R10,    [R8+8]              ;R10 contiene Y buscado    
-    buscarOcasEnPosicion    RSI,    qword[RDI],     R9,     R10,    1
+    traducirLetra           [dirPosicionOrigen]
+    traducirLetra           [dirPosicionDestino]
+    guardarDatos
 
-    cmp RAX,1
-    jne finValidacion
-    
-    ;2.-validar que hayan cero personajes en la posicion destino
-    ;ocas
-    mov     RDI,    [dirInfoOcas]              
-    mov     R8,     [dirPosicionDestino]
-    mov     R9,     [R8]                ;R9 contiene X buscado
-    mov     R10,    [R8+8]              ;R10 contiene Y buscado 
-    buscarOcaEnPosicion     RSI,    qword[RDI],     R9,     R10,    1
-    cmp RAX,0
-    jne finValidacion
-
-    validarAnvance:;se trata de ver que la oca solo se estè moviendo de
+    unaOcaEnOrigen
+    sinOcasEnDestino
+    sinZorroEnDestino
+    movimientoAdelanteOCostado    
 
 validacionLogicaMovZorro:
-    traducirLetra [dirPosicionDestino]
-    ;no olvidar iniciarlizar posicionOrigen con la posicion que indica el vector de info del zorro
+    traducirLetra           [dirPosicionDestino]
+    ;copio dentro de lo que apunta dirPosicionOrigen el valor de la posicion actual del zorro
+    ;tambien guardo localmente la direccion de la posicion actual del zorro
+
+;con esto parece que estoy inicializando x_origen y y_origen localmente
+
+    ;guardo en registros los valores de la posicion actual del zorr
+    mov RAX, [dirInfoZorro]
+    mov RBX, [RAX+8];->RBX contiene la posFilZorro
+    mov RDX, [RAX+16];->RDX contiene la posColZorro
+    ;ya tengo en el RBX X_origen y en 
+    mov [x_origen],RBX
+    mov [y_origen],RDX
+
+;ahora falta guardar en donde apunta dirPosicionOrigen
+    mov RDI, [dirPosicionOrigen];->RDI tiene la direccion de coordenadasOrigen
+    mov [RDI],RBX
+    mov [RDI+8],
 
 
 finValidacion:
@@ -132,5 +123,5 @@ finValidacion:
 fallaValidacion:
     mov     RDI,    mensajeInputInvalido
     mPuts
-    jmp     lecturaInput
+    jmp     pedirInput
 
