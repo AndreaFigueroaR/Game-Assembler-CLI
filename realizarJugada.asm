9313e08c8c0a680e147ca582ca34f0288f561a2c
leticiafrR <129856789+leticiafrR@users.noreply.github.com>
2024-06-21 23:16:46 -0300

    Update realizarJugada.asm

--- realizarJugada.asm
+++ realizarJugada.asm
@@ -7,14 +7,18 @@
 
 global realizarJugada
 section     .data
-    murioOca            db      0
+    esSalto             dq      0;->0 no es salto, 1 es salto
 
 section     .bss
     ;por dato
     jugadorActual           resq    1   ;0 -> turno zorro, 1 -> turno ocas.
     infoCoordenadas         times   0   resb        ; Vector de la forma: [filOrigen, colOrigen, filDestino,colDestino]
-        coordenadasOrigen               resq 2
-        coordenadasDestino              resq 2
+        coordenadasOrigen   times   0   resb
+            filOrigen                   resq 1
+            colOrigen                   resq 1
+        coordenadasDestino  times   0   resb
+            filDestino                  resq 1
+            colDestino                  resq 1
     cantidadOcasVivas       resq    1
     ;punteros
     dirCantidadOcasVivas    resq    1   
@@ -24,66 +28,71 @@ section     .bss
     dirEstadisticas         resq    1
       
     ;auxiliares
+    sentidoJugada           times   0    
+        sentidoFila                 resq    1   ;0 si es es en la misma fila, 1 si en una fila superior, -1 para fila inferior
+        sentidoColumna              resq    1   ;0 si es es en la misma columna, 1 si en una columna superior, -1 para columna inferior
     desplazVector                   resq    1
     cantElemVector                  resq    1
     dirBaseVector                   resq    1
     dirDestinoVector                resq    1
-    coordenadasBuscadas times   0   
-        filBuscada                  resq    1
-        colBuscada                  resq    1
-
+    coordenadasAux          times   0   
+        filAux                  resq    1
+        colAux                  resq    1
 
 realizarJugada:
     guardarDatos
-    ;moviendo jugador actual
     cmp                     qword[juagadorActual],0 ;->0 si es turno del zorro
-    jne                     cambiarPosOcas
-    mov                     rax,[dirPosicionZorro]
-    copiarVector            2,coordenadasDestino,rax
-    jmp                     ejecutarMovimiento
-    cambiarPosOcas:
-    buscarOcaPorCoordenadas coordenadasOrigen ;->
-    mov                     rax,[dirPosicionesOcas]
-	add                     rax,[desplazVector]
-	copiarVector            2,coordenadasDestino,rax
-    
-
-
-    
-    ;necesito encontrar la direcciòn de la posiciòn de la oca que tiene  la misma posicion que coordenadas origen y a esa direcciòn llevarme la coordenada destino
+    jne                     cambiarPosOca
+    modificarPosZorro       
     ejecutarMovimiento:
-    ;se actualiza la info del jugador actual en base del las coordenadas copiadas
-
-    ;si el jugador actual es el zorro se debe guardar lo que tenga como posicion actual en su info en la parte de posicionOrigen
-    
-    ;si es oca-> se cambia la posicion actual de la oca que indiue que està en el mismo liugar que la posicion de origen... y nada màs
-    ;si es zorro-> si la nueva posicion es de un salto entonces se debe encontrar la oca que este en la posicion en el medio de estas(oca comida), se pisa la su posicion con la de la ultima oca viva, se disminuye la cantidad de ocas vivas, se cambia la posicion del zorro a la que indica la posicionDestino y se setea como 1 a "murioOca".
-    ;               si no era un salto solo se cambia la psocion del zorro a la posicion indicada en posicionDestino
-    
-    ;ejecutadas las consecuencias del movimiento sobre el juego: 
+    definirSaltoYSentidoMovida
+    cmp                     qword[esSalto],0
+    je                      cambiarJugador
+    mMatarOca
+    jmp                     cambiarJugador
+    cambiarPosOca:
+    modificarPosOca 
+    cambiarJugador:         
     ;llamamos a estadisticas y actualizamos jugadorActual:       
         ;si murioOca==0 -> cambiamos de jugadorActual al opuesto
         ;si  murioOca!=0 -> se deja el jugadorActual
 
     ;fin
-
 ;__________________________________________________________
 ;**********************************************************
 ;                   RUTINAS INTERNAS
 ;__________________________________________________________
 ;**********************************************************
-
-buscarOca::
+matarOca:
+;sumo el sentido de la jugada (a la posicion origen) y busca una oca con esa posiciòn
+    mov                 rax,[sentidoFila]
+    add                 [filOrigen],rax
+    mov                 rax,[sentidoColumna]
+    add                 [colOrigen],rax
+    ;ahora coordenadas origen tiene la posicion de la oca que busco eliminar 
+    buscarOcaPorCoordenadas coordenadasOrigen
+    ;desplazVector me dice què oca matar >:)
+    mov                 rax,[dirPosicionesOcas]
+    add                 rax,[desplazVector]
+    ;busco direcciòn de la ultima oca viva
+    dec                 qword[cantidadOcasVivas]        ;mato una oca
+    imul                r8,qword[cantidadOcasVivas],16  ;cada coordenada ocupa 16 bytes (tengo desplazamiento hasta la ùltima viva)
+    add                 r8,[dirPosicionesOcas]          ;tengo la direcciòn de la ultima oca viva          
+    copiarVector        2,r8,rax   
+    ret
+;__________________________________________________________
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+buscarOca:
     ;si ya la encontrè no sigo buscando=>el desplazamiento me dirà la direcciòn que le tengo que mnadar a copiar vector
     mov             r8,[desplazVector]
     mov             r9,[dirPosicionesOcas+r8];->tengo la fila
     ;si la fila no coincide->sgt oca
-    cmp             r9,[filBuscada]
+    cmp             r9,[filAux]
     jne             sgtOca
     ;si la columna no coincide->sgt oca
     add             r8,8
     mov             r9,[dirPosicionesOcas+r8];->tengo la columna
-    cmp             r9,[colBuscada]
+    cmp             r9,[colAux]
     jne             sgtOca
     ;encontrè coincidencia=>salgo del loop
     jmp             ocaEncontrada
@@ -113,4 +122,58 @@ guardarVector:
     dec         qword[cantElemVector]
     jmp         copiarSgt
     endCopiado:
+    ret
+;__________________________________________________________
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+analizarMovida:
+    ;POST: setea el sentido de la jugada y si es que es salto o no
+    ;coordenadas destino - coordenas origen
+    mov     rax,[filOrigen];<-tiene Xdestino
+    sub     [filDestino],rax;<-resta a las coordenadas de origen
+    mov     rax,[colOrigen]
+    sub     [colDestino],rax
+
+    cmp     qword[filDestino],0
+    je      mismaFila
+    jg      filaSuperior
+    mov     qword[sentidoFila],-1
+    ;valor Abs
+    not     qword[filDestino]
+    inc     qword[filDestino]
+    jmp     analizarCol
+    mismaFila:
+    mov     qword[sentidoFila],0
+    jmp     analizarCol
+    filaSuperior:
+    mov     qword[sentidoFila],1
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    analizarCol:
+    cmp     qword[colDestino],0
+    je      mismaCol
+    jg      colSuperior
+    mov     qword[sentidoColumna],-1
+    ;valor Abs
+    not     qword[colDestino]
+    inc     qword[colDestino]
+    jmp     verificarSalto
+    mismaCol:
+    mov     qword[sentidoFila],0
+    jmp     verificarSalto
+    colSuperior:
+    mov     qword[sentidoFila],1
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    verificarSalto:
+    cmp     qword[filDestino],2
+    je      verificarSaltoCol
+    cmp     qword[filDestino],0
+    je      verificarSaltoCol
+    jmp     finAnalizarMovida
+    verificarSaltoCol:
+    cmp     qword[colDestino],2
+    je      saltoConfirmado
+    cmp     qword[colDestino],0
+    jne     finAnalizarMovida
+    saltoConfirmado:
+    mov     qword[esSalto],1
+    finAnalizarMovida:
     ret
